0a8e83cc7c24224891be9b769a8faeaf2b489b71
Solomon Gachure <gachuresolomon64@gmail.com>
2023-09-22 08:44:46 +0300

    Added the work page that is to contain the projects that i have worked on

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import About from "./components/about";
 import Navbar from "./components/navbar"
 import Skills from "./components/skills";
+import Work from "./components/work";
 function App() {
   return (
     <div>
       <Navbar/>
       <About/>
       <Skills/>
+      <Work/>
     </div>
   );
 }
---
+++ src/components/work.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div class='w-full h-screen bg-black'>
+          <div class='max-w-[1000px] flex flex-col justify-center px-8 mx-auto'>
+              <h1 className='text-6xl text-gray-300 font-bold inline border-b-2 border-yellow-300'>Work</h1>
+            <p class='text-gray-300 py-2'>Here are some of my projects that I have worked on:</p>
+        </div>
+    </div>
+  )
+}
+
+export default Work
