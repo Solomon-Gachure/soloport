975056bb102d22738ff0aaf89090120ad16954ec
Solomon Gachure <gachuresolomon64@gmail.com>
2023-09-23 17:20:18 +0300

    Some slight changes to the work page on small screen

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div name='work' class='w-full h-screen bg-black'>
-          <div class='max-w-[1000px] flex flex-col justify-center px-8 mx-auto'>
-              <h1 className='text-6xl text-gray-300 font-bold inline border-b-2 border-yellow-300'>Work</h1>
-            <p class='text-gray-300 py-2'>Here are some of my projects that I have worked on:</p>
+    <div name='work' className='w-full min-h-screen bg-black'>
+          <div className='max-w-[1000px] flex flex-col justify-center px-8 mx-auto h-full'>
+            <div className='pb-8'>
+          <h1 className='text-6xl text-gray-300 font-bold inline border-b-2 border-yellow-300'>Work</h1>
+          <p className='text-gray-300 py-2'>Here are some of my projects that I have worked on:</p>
+            </div>
+              
         </div>
     </div>
   )
